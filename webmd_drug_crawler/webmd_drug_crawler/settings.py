c881ae0e40678859eeec3b7d65b0fa6af7faf015
Hemant <hemantksingh246@gmail.com>
2020-04-10 21:03:34 -0700

    DEBUG log suppressed from printing

--- webmd_drug_crawler/webmd_drug_crawler/settings.py
+++ webmd_drug_crawler/webmd_drug_crawler/settings.py
@@ -38,6 +38,9 @@ DOWNLOAD_DELAY = 2
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
+# print only INFO logs not DEBUG logs
+LOG_LEVEL = 'INFO'
+
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
